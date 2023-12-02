277e126ac9822226781b08b6eeba1badffe38d71
Ritik Upadhyay <116427440+ritiku2004@users.noreply.github.com>
2023-12-02 15:15:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,19 +19,4 @@ window.onscroll = () => {
   menuIcon.classList.remove('bx-x');
   nav.classList.remove('active');
 }
-function sendMail() {
-    let top = document.getElementById("nm").value;
-    let bdy ="<br/>Name - " + document.getElementById("nm").value + "<br/><br/>mail id - " + document.getElementById("mail").value + " " +"<br/><br/>Mobile number - " + document.getElementById("mob").value + " " + "<br/><br/>Message - " + document.getElementById("data").value +"<br/>";
-    Email.send({
-        Host: "smtp.elasticemail.com",
-        Username: "visiters2004@gmail.com",
-        Password: "88879B73FD61A3B63466670E6283B72D454C",
-        To: 'ritikupadhyay838@gmail.com',
-        From: 'ritikupadhyay838@gmail.com',
-        Subject: top,
-        Body: bdy
-        }).then(
-        message => alert("Message sent successfully.")
-         );
-     }
-}
+
